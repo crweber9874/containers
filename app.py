0899a39b795c31a340defd54f8897d348631c719
crweber9874 <christopher.weber9874@gmail.com>
2023-05-30 18:15:10 -0700

    #

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
-from jabebot import ask, append_interaction_to_chat_log
+from bot import ask, append_interaction_to_chat_log
 app = Flask(__name__)
 # if for some reason your conversation with Jabe gets weird, change the secret key
 app.config['SECRET_KEY'] = 'clyde'
