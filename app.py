70eab30fc28ba35b199c733a207c6dbdf4fb80e3
crweber9874 <christopher.weber9874@gmail.com>
2023-05-30 17:55:27 -0700

    #

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, session
-import twilio
+from twilio.twiml.messaging_response import MessagingResponse
 from bot import ask, append_interaction_to_chat_log
 
 app = Flask(__name__)
